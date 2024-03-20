7c8c64e219b95637e056d6a1b3ffe80d5ae4704a
Mahmoud Elkazzaz <mhkazazz@hotmail.com>
2024-03-20 17:40:33 +1100

    Adding projects, job description and removing the contact page

--- js/main.js
+++ js/main.js
@@ -1,29 +1,38 @@
-const menuBtn = $('.menu-btn');
-const hamburger = $('.menu-btn_burger');
-const nav = $('.nav');
-const navMenu = $('.nav-menu');
-const navItems = $('.nav-menu_item');
+const menuBtn = $(".menu-btn");
+const hamburger = $(".menu-btn_burger");
+const nav = $(".nav");
+const navMenu = $(".nav-menu");
+const navItems = $(".nav-menu_item");
+const navLinks = $(".nav-menu_link");
 
 let showMenu = false;
 
+window.addEventListener("scroll", function () {
+  if (window.scrollY !== 0) {
+    navLinks.addClass("navLinksScroll");
+    navMenu.addClass("navMenuScroll");
+  } else {
+    navLinks.removeClass("navLinksScroll");
+    navMenu.removeClass("navMenuScroll");
+  }
+});
 
 toggleMenu = () => {
-  if(!showMenu) {
-    hamburger.addClass('open');
-    nav.addClass('open');
-    navMenu.addClass('open');
-    navItems.addClass('open');
+  if (!showMenu) {
+    hamburger.addClass("open");
+    nav.addClass("open");
+    navMenu.addClass("open");
+    navItems.addClass("open");
 
     showMenu = true;
-
   } else {
-    hamburger.removeClass('open');
-    nav.removeClass('open');
-    navMenu.removeClass('open');
-    navItems.removeClass('open');
+    hamburger.removeClass("open");
+    nav.removeClass("open");
+    navMenu.removeClass("open");
+    navItems.removeClass("open");
 
     showMenu = false;
   }
-}
-menuBtn.on( "click", toggleMenu );
+};
+menuBtn.on("click", toggleMenu);
 //
